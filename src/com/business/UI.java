1ea6cce092bf7ec59a90ec481cfe518a32f77738
NguyeLongT <nguyelongt@gmail.com>
2017-09-13 01:51:33 -0400

    Fixed UI new line

--- src/com/business/UI.java
+++ src/com/business/UI.java
@@ -10,7 +10,7 @@ public class UI {
     public static void menuList() {
         consolePrint("\nCOMMAND MENU\n");
         consolePrint("List - List All Stores\n");
-        consolePrint("Div - List All Stores for a Division");
+        consolePrint("Div - List All Stores for a Division\n");
         consolePrint("Sales - Sales Summary\n");
         consolePrint("Help - Show This Menu\n");
         consolePrint("Exit - Exit This Application\n");
